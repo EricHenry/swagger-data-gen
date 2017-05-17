c94d48275387e852b52563463cd96dac907d4cb7
Eric Correia <correia.eh@gmail.com>
2017-05-17 15:18:18 -0400

    Rearchitecture

--- index.js
+++ index.js
@@ -3,86 +3,27 @@
 'use strict';
 
 // import dependencies
-var fs = require('fs');
-var readline = require('readline');
-var ArgumentParser = require('argparse').ArgumentParser;
-var forEach = require('lodash').forEach;
-var SwaggerParser = require('swagger-parser');
-var jsf = require('./lib/jsfConfig.js');
-var requireAllProperties = require('./lib/utils/helpers.js').requireAllProperties;
+const fs = require('fs');
+const readline = require('readline');
+const ArgumentParser = require('argparse').ArgumentParser;
 const SwaggerDataGenerator = require('./lib/sdg.js');
 
 // grab expected user input
-var parser = new ArgumentParser({
+const parser = new ArgumentParser({
   addHelp: true,
-  description: 'Swagger Data Generator generates mock data from Swagger files.',
+  description: 'Swagger Data Generator generates mock data from Swagger files.'
 });
-var args;
+let args;
 parser.addArgument(['-y'], { help: 'Always overwrite output file (do not ask to overwrite)', action: 'storeTrue', dest: 'force-yes' });
 parser.addArgument(['swagger-input'], { help: 'Input Swagger file' });
 parser.addArgument(['json-output'], { help: 'Output file for generated mock data' });
 args = parser.parseArgs(process.arguments);
 
-
 const sdg = new SwaggerDataGenerator(args['swagger-input']);
 
-sdg.build()
-  .then(() => saveOutput(sdg.generateMock()))
-// SwaggerParser.parse(args['swagger-input'])
-
-//   // parse the data and make sure all the properties are required.
-//   // they need to be required so JSF creates mock data for all properties
-//   .then(successfulParse)
-//   .catch(unSuccessfulParse)
-
-//   // make sure there are not any references in the definitions and create the mock data
-//   .then(dereferencedSuccess);
-
-// *******************************************************
-// Helper Functions
-// *******************************************************
-
-/**
- * successfulParse - Massage data to require all definiton properties then dereference the api
- *
- * @param parsedApi {Object} - a swagger api in the form of an Object
- * @returns {Object} - A Promise Object for handling dereferencing the api
- */
-function successfulParse(parsedApi) {
-  var swaggerDoc = parsedApi;
-
-  swaggerDoc.definitions = requireAllProperties(swaggerDoc.definitions);
-
-  return SwaggerParser.dereference(swaggerDoc);
-}
-
-/**
- * unSuccessfulParse - an error has occured when parsing, update the user
- *
- * @param err {Object} - an error object describing the error
- */
-function unSuccessfulParse(err) {
-  console.log(err);
-}
-
-/**
- * dereferencedSuccess - all $ref were successfully removed from the defintions
- *
- * @param dereferencedApi {Object} - a swagger api in the form of an Object
- */
-function dereferencedSuccess(dereferencedApi) {
-  var generatedSwaggerData = {};
-
-  forEach(dereferencedApi.definitions, function generateData(definition, name) {
-    try {
-      generatedSwaggerData[name] = jsf(definition);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
-  saveOutput(generatedSwaggerData);
-}
+sdg
+  .run()
+  .then(() => saveOutput(sdg.generateMocks()));
 
 /**
  * saveOutput - Verify output path and save file
@@ -92,7 +33,7 @@ function dereferencedSuccess(dereferencedApi) {
 function saveOutput(generatedData) {
   var rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
 
   function writeData(yes) {
@@ -115,3 +56,5 @@ function saveOutput(generatedData) {
       });
   }
 }
+
+module.exports = SwaggerDataGenerator;
